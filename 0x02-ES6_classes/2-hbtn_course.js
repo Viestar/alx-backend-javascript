6b1792dccceca2e90a1c5f9191450587b5cc422e
Viestar <m.silverbuckson@gmail.com>
2023-11-30 18:00:44 +0300

    2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,56 +1,61 @@
-// Validation functions
-function validateLength(length) {
-  const parsedLength = parseInt(length, 10);
-  if (Number.isNaN(parsedLength) && typeof parsedLength === 'number') {
-    return parsedLength;
-  }
-  throw new TypeError('Length must be a number');
-}
+class HolbertonCourse {
+  constructor(name = '', length = 0, students = []) {
+    if (typeof students !== 'object') {
+      throw TypeError('Students must be an array');
+    }
+    for (const student of students) {
+      if (typeof student !== 'string') {
+        throw TypeError('Student must be a string');
+      }
+    }
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw TypeError('Length must be a number');
+    }
 
-function validateName(name) {
-  if (typeof name === 'string') {
-    return name;
-  }
-  throw new TypeError('Name must be a string');
-}
-
-function validateStudents(students) {
-  if (Array.isArray(students) && students.every((student) => typeof student === 'string')) {
-    return students;
-  }
-  throw new TypeError('Students must be an array of strings');
-}
-
-export default class HolbertonCourse {
-  // Constructor with validators
-  constructor(name, length, students) {
-    this._name = validateName(name);
-    this._length = validateLength(length);
-    this._students = validateStudents(students);
+    this._students = students;
+    this._name = name;
+    this._length = length;
   }
 
-  // Getters and setters for the attributes.
-  get name() {
-    return this._name;
-  }
-
-  set name(newName) {
-    this._name = validateName(newName);
-  }
-
-  get length() {
-    return this._length;
-  }
-
-  set length(newLength) {
-    this._length = validateLength(newLength);
+  set students(students = []) {
+    if (typeof students !== 'object') {
+      throw TypeError('Students must be an array');
+    }
+    for (const student of students) {
+      if (typeof student !== 'string') {
+        throw TypeError('Student must be a string');
+      }
+    }
+    this._students = students;
   }
 
   get students() {
     return this._students;
   }
 
-  set students(newStudents) {
-    this._students = validateStudents(newStudents);
+  set name(name = '') {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set length(length = 0) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
+  }
+
+  get length() {
+    return this._length;
   }
 }
+export default HolbertonCourse;
