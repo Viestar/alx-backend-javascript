26eebfa4b66b0270b6549db97980a97a3a09ef29
Viestar <m.silverbuckson@gmail.com>
2023-11-30 19:33:29 +0300

    7-airport.js

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,14 @@
+export default class AirPort {
+  constructor(name, code) {
+    if (typeof name === 'string') {
+      this._name = name;
+    }
+    if (typeof code === 'string') {
+      this._code = code;
+    }
+  }
+
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+}
