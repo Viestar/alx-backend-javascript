8bb6040e6085b16aac873aaf0a42c2d98a516bbb
Viestar <m.silverbuckson@gmail.com>
2023-12-06 05:51:22 +0300

    7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,4 @@
-// function named hasValuesFromArray that returns a boolean 
+// function named hasValuesFromArray that returns a boolean
 // if all the elements in the array exist within the set.
 export default function hasValuesFromArray(set, array) {
   for (const item of array) {
