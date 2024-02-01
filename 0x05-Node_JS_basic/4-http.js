a5b6d196337b8a224b3e5780cc5682b5c71e9a26
Viestar <m.silverbuckson@gmail.com>
2024-02-01 07:19:48 +0300

    4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,14 +1,24 @@
 const http = require('http');
 
+// Define the port number
 const port = 1245;
 
+// Create an HTTP server
 const app = http.createServer((req, res) => {
+  // Set the status code and content type in the HTTP response header
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+
+  // Send the response body
   res.end('Hello Holberton School!');
 });
 
+// Start the server and listen on the specified port
 app.listen(port, () => {
+  // Callback function executed when the server starts
+  console.log(`Server running at http://localhost:${port}/`);
 });
 
+// Export the app variable for external use (e.g., testing)
 module.exports = app;
+
